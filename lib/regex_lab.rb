3e024f815d8bde7032255e1a7ffad17d90a83510
Felice Forby <feliceforby@gmail.com>
2018-04-27 23:46:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiou]/i) ? true : false
+  # word.match(/\A[aeiou]/i) ? true : false
+  word.match(/^[aeiou]\w*/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
